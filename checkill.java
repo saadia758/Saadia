fc38c2c4fb47b8d0d5324ddcfd5a187ee3cf0523
Saadia Tabassum <saadiatabassum1234@gmail.com>
2020-05-22 21:35:18 +0530

    Create checkill.java
    first change:All year is divisible by 400 are leap year.
    second change: All year divisible by 100 but not by 400 are not leap year.
    third change: All yaer divisible by 4 but not by 100 are laeo year.
    fourth change: All year not divisible by 4 are not leap year.

---
+++ checkill.java
@@ -0,0 +1,45 @@
+package Saadia;
+
+import java.util.Scanner;
+
+public class checkill {
+
+	public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        System.out.print("Enter any year:");
+	        int year = s.nextInt();
+	        boolean flag = false;
+	        if(year % 400 == 0)
+	        {
+	            flag = true;
+	        }
+	        else if (year % 100 == 0 && year%400!=0) 
+	        {
+	            flag = false;
+	        }
+	        else if(year % 4 == 0 && year%100!=0)
+	        {
+	            flag = true;
+	        }
+	        else if(year%4!=0)
+	        {
+	            flag = false;
+	        }
+	        else
+	        {
+	        	flag=true;
+	        }
+	        if(flag)
+	        {
+	            System.out.println("Year "+year+" is a Leap Year");
+	        }
+	        else
+	        {
+	            System.out.println("Year "+year+" is not a Leap Year");
+	        }
+	    }
+		
+
+	}
+
+
